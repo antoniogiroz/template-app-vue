f85d65efae1d1f8cdba2ef70cf02e975b9c4de8c
Antonio Gil <antonioluisgil@gmail.com>
2023-04-02 16:06:05 +0200

    chore: add unocss and autoimport plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
+import unocss from 'unocss/vite';
+import autoImport from 'unplugin-auto-import/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    autoImport({
+      imports: ['vue', 'vue-router', '@vueuse/core'],
+      eslintrc: {
+        enabled: true,
+        filepath: '.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+      dts: true,
+      vueTemplate: true,
+    }),
+
+    unocss(),
+  ],
 
   resolve: {
     alias: {
